d69ca51f8e53757c96fd1015933ce7f264a4f258
agolombek <aron.golombek@student.uva.nl>
2023-02-02 18:00:17 +0100

    Update gold_ownership_animation.py

--- Data/gold_ownership_animation.py
+++ Data/gold_ownership_animation.py
@@ -26,6 +26,8 @@ df = pd.read_csv('./data_None.csv',delimiter=",")
 # index 8 is number of trades
 
 gold_amounts = []
+max_gold = []
+avg_gold = []
 n_bins = 50
 step= 1
 
@@ -40,6 +42,8 @@ for index, row in df.iterrows():
             
 
     if row[0] == 744:
+        max_gold.append(np.max(gold_amounts))
+        avg_gold.append(np.mean(gold_amounts))
             
         # Plot Histogram of every Step
         plt.hist(gold_amounts , range=(0,50), bins=n_bins)
@@ -48,15 +52,25 @@ for index, row in df.iterrows():
         plt.ylabel("Number of Agents")
         plt.ylim((0,300))
         plt.tight_layout()
-        plt.savefig(f'gold_histograms/gold_possessions_step_{step}.png')
+        # plt.savefig(f'gold_histograms/gold_possessions_step_{step}.png')
         plt.show()
         gold_amounts = []
         step += 1
 
-steps = step
-filenames = [f'gold_histograms/gold_possessions_step_{step}.png' for step in range(1, steps)]
-with imageio.get_writer('gold_possession.gif', mode='I', duration=0.2) as writer:
-    for filename in filenames:
-        image = imageio.imread(filename)
-        writer.append_data(image)
-writer.close()
+# steps = step
+# filenames = [f'gold_histograms/gold_possessions_step_{step}.png' for step in range(1, steps)]
+# with imageio.get_writer('gold_possession.gif', mode='I', duration=0.2) as writer:
+#     for filename in filenames:
+#         image = imageio.imread(filename)
+#         writer.append_data(image)
+# writer.close()
+
+plt.plot(np.arange(step-1), max_gold, label='max gold')
+plt.plot(np.arange(step-1), avg_gold, label='avergae gold')
+plt.title('Agent Gold Ownership')
+plt.legend()
+plt.xlabel("step")
+plt.ylabel("amount of gold")
+plt.grid('both')
+# plt.savefig("agent_gold_dyn")
+plt.show()
